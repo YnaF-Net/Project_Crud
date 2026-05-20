0997a7c4285a485585d586d7b4a2dd1fa5bb646b
YnaF-Net <84fanyprayoga@gmail.com>
2026-05-20 15:24:08 +0700

    update

---
+++ pages/user.php
@@ -0,0 +1,56 @@
+<?php
+$selectUser = mysqli_query($koneksi, "SELECT * FROM users");
+$rows = mysqli_fetch_all($selectUser, MYSQLI_ASSOC);
+
+?>
+
+<div class="card">
+    <div class="card-header text-center">
+        <h2 class="card-title">Users</h2>
+    </div>
+    <div class="card-body">
+        <div class="mb-2">
+            <a href="?page=user-create-edit" class="btn btn-primary">Create</a>
+        </div>
+        <div class="table-responsive">
+            <?php
+            if (isset($_GET['status']) && $_GET['status'] == 'success') {
+                $status = "Data Berhasil ditambah!";
+                $location = "?page=user";
+                echo statusSuccess($status, $location);
+            }
+            ?>
+            <table class="table table-bordered">
+                <thead class="text-center">
+
+
+                    <tr>
+                        <th>No</th>
+                        <th>Nama</th>
+                        <th>Email</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    foreach ($rows as $index => $r) {
+                    ?>
+                        <tr>
+                            <td><?= $index + 1 ?></td>
+                            <td><?= $r['name'] ?></td>
+                            <td><?= $r['email'] ?></td>
+                            <td>
+                                <a href="?page=user-create-edit&idEdit=<?= $r['id'] ?>" class="btn btn-success">Edit</a>
+                                <form action="" method="post" class="d-inline">
+                                    <button class="btn btn-danger">Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                    <?php
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
